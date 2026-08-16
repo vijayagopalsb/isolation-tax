fba9422dbfd215c237f024d46ad75e9db500e4b6
Vijayagopal <vijayagopal.sb@gmail.com>
2026-08-17 01:04:32 +0530

    Update: dependency violation implememted

--- demo-microservices/service-a/src/main/java/com/isolation/servicea/controller/ServiceAController.java
+++ demo-microservices/service-a/src/main/java/com/isolation/servicea/controller/ServiceAController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-//import com.isolation.serviceb.ServiceBApplication;
+import com.isolation.serviceb.ServiceBApplication;
 
 @Slf4j
 @RestController
@@ -14,7 +14,7 @@ import java.util.Map;
 public class ServiceAController {
 
     // This creates a real compiled dependency on ServiceBApplication
-    //private final ServiceBApplication serviceBApplication = new ServiceBApplication();
+    private final ServiceBApplication serviceBApplication = new ServiceBApplication();
 
     @GetMapping("/resource/{id}")
     public ResponseEntity<Map<String, Object>> getResource(@PathVariable String id) {
